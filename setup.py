c65490d57f608c5bede40f76081d0ef4769c1f99
lewisleedev <lewislee@lewislee.net>
2021-07-24 06:26:33 +0000

    build: bump vesion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='reddit2md',
-    version='0.1.0',
+    version='0.2.0',
     author='Lewis Lee',
     author_email='lewislee@lewislee.net',
     packages=['reddit2md'],
